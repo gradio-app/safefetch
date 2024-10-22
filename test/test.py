e612f19d4453df64e1a92131a83a9907c21ee327
Abubakar Abid <abubakar@huggingface.co>
2024-10-22 10:10:28 -0700

    add tests

---
+++ test/test.py
@@ -0,0 +1,32 @@
+import pytest
+
+from safehttpx import get
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "url",
+    [
+        "https://localhost",
+        "http://127.0.0.1/file/a/b/c",
+        "http://[::1]",
+        "https://192.168.0.1",
+        "http://10.0.0.1?q=a",
+        "http://192.168.1.250.nip.io",
+    ],
+)
+async def test_local_urls_fail(url):
+    with pytest.raises(ValueError, match="failed validation"):
+        await get(url)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "url",
+    [
+        "https://google.com",
+        "https://8.8.8.8/",
+        "http://93.184.215.14.nip.io/",
+        "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/luigi/luigi.ply",
+    ],
+)
+async def test_public_urls_pass(url):
+    await get(url)
